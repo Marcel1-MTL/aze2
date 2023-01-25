1e33c57e8a0d02d3442bed75ffcdbf628eee06a7
Marcel1-MTL <123545925+Marcel1-MTL@users.noreply.github.com>
2023-01-25 17:33:47 +0100

    Mise à jour pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,3 +1,8 @@
+radio.onReceivedNumber(function (receivedNumber) {
+    if (receivedNumber == 12) {
+        basic.showString("switch")
+    }
+})
 basic.forever(function () {
-	
+    radio.setGroup(88)
 })
